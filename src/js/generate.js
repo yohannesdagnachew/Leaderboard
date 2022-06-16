7aa0d149c1b6a0f4f0470ba4b3b4c77fe30e24d7
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-06-16 22:22:52 +0300

    Add hr to text titile

--- src/js/generate.js
+++ src/js/generate.js
@@ -11,7 +11,7 @@ const generate = async () => {
         if (user.index % 2 === 0) {
           return `<li class='list-item'><p>${user.user}</p><p>${user.score}</p></li>`;
         }
-          return `<li class='gray'><p>${user.user}</p><p>${user.score}</p></li>`;
+        return `<li class='gray'><p>${user.user}</p><p>${user.score}</p></li>`;
       });
       const list = document.querySelector('#list');
       list.innerHTML = `${html.join('')}`;
