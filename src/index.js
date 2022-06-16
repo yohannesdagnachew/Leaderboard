4e8358c1c088bcb330294dc26149944de8e17ab9
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-06-16 15:52:10 +0300

    hit the API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './style/main.css';
+import { getPost } from './js/createlement.js';
+import { generate } from './js/generate';
+
+const url =
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ljmGTCiQ0bLn1cM60koG/scores';
+const add = document.querySelector('#submit');
+const refbtn = document.querySelector('#refresh');
+
+generate();
+
+const newFunc = () => {
+  const user = document.querySelector('#name');
+  const score = document.querySelector('#score');
+  const inputFile = {
+    user: user.value,
+    score: score.value,
+  };
+  getPost(inputFile);
+};
+
+add.addEventListener('click', (e) => {
+  e.preventDefault();
+  newFunc();
+});
+
+refbtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  generate();
+});
