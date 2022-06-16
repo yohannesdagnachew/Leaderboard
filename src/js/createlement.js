9006728e665fa5224b76332a13cce85eed481bc1
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-06-16 16:05:40 +0300

    Fix linter

--- src/js/createlement.js
+++ src/js/createlement.js
@@ -1,13 +1,13 @@
-
-const url= 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ljmGTCiQ0bLn1cM60koG/scores'
-export const getPost = async (inputFile) => {
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(inputFile),
-      });
-      await response.json();
-}
-
+const url =
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ljmGTCiQ0bLn1cM60koG/scores';
+const getPost = async (inputFile) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(inputFile),
+  });
+  await response.json();
+};
+exports.getPost = getPost;
--- src/js/generate.js
+++ src/js/generate.js
@@ -1,13 +1,15 @@
-
-const url= 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ljmGTCiQ0bLn1cM60koG/scores'
-export const generate = async () => { 
-    await fetch(url).then(res=>{
-         return res.json();     
-     }).then(file =>{
-         const html = file.result.map(user => {
-             return `<li class='list-item'><p>${user.user}</p><p>${user.score}</p></li>`
-         })
-         const list = document.querySelector('#list')
-         list.innerHTML=`${html.join('')}`
-     })
- }
+const url =
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ljmGTCiQ0bLn1cM60koG/scores';
+export const generate = async () => {
+  await fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((file) => {
+      const html = file.result.map((user) => {
+        return `<li class='list-item'><p>${user.user}</p><p>${user.score}</p></li>`;
+      });
+      const list = document.querySelector('#list');
+      list.innerHTML = `${html.join('')}`;
+    });
+};
