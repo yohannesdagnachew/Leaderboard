30b285c706f8e5d08f31ae0143f20a8917656c7d
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-06-16 22:14:55 +0300

    Edit linter

--- src/js/generate.js
+++ src/js/generate.js
@@ -1,18 +1,17 @@
-const url ='https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ljmGTCiQ0bLn1cM60koG/scores';
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ljmGTCiQ0bLn1cM60koG/scores';
 const generate = async () => {
   await fetch(url)
     .then((res) => res.json())
     .then((file) => {
       const newresult = file.result;
-      for (let i = 0; i < newresult.length; i++) {
+      for (let i = 0; i < newresult.length; i += 1) {
         newresult[i].index = i;
       }
       const html = newresult.map((user) => {
-        if (user.index % 2 == 0) {
+        if (user.index % 2 === 0) {
           return `<li class='list-item'><p>${user.user}</p><p>${user.score}</p></li>`;
-        } else {
-          return `<li class='gray'><p>${user.user}</p><p>${user.score}</p></li>`;
         }
+          return `<li class='gray'><p>${user.user}</p><p>${user.score}</p></li>`;
       });
       const list = document.querySelector('#list');
       list.innerHTML = `${html.join('')}`;
