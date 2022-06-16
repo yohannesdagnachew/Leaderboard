3f6a567f541a0a5fb3c340bf1ae0a9ebefefe97b
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-06-16 16:28:17 +0300

    Fix linter

--- src/js/generate.js
+++ src/js/generate.js
@@ -1,8 +1,7 @@
 const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ljmGTCiQ0bLn1cM60koG/scores';
 const generate = async () => {
   await fetch(url)
-    .then((res) => res.json()
-    )
+    .then((res) => res.json())
     .then((file) => {
       const html = file.result.map((user) => `<li class='list-item'><p>${user.user}</p><p>${user.score}</p></li>`);
       const list = document.querySelector('#list');
